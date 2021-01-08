a80c33294053205f903ecaa92d23bd95b8300329
kemiranda <35238395+kemirandad@users.noreply.github.com>
2021-01-08 17:32:09 -0500

    Personalizando icono de PlatziConf en GoogleMaps

--- app/src/main/java/com/example/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/example/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,14 +1,22 @@
 package com.example.conf.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.conf.R
+import com.example.conf.model.Ubication
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -68,7 +76,25 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
             }
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitud, ubication.longitud)
 
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitud, ubication.longitud)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+
+        markerOptions.title("Platzi Conf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let {
+            ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150, 150, false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap?.addMarker(markerOptions)
     }
 }
